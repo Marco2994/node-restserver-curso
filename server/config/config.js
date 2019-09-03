cd591baf399f2f1654b8956f4b7aa31c54539153
Marco <marco_al94@hotmail.com>
2019-09-02 23:20:45 -0500

    Cambio One

--- server/config/config.js
+++ server/config/config.js
@@ -14,11 +14,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=====================================
 
 let urlDB;
-/*
+
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/Cafe'
 } else {
-    urlDB = 'mongodb://admin:admin@cluster0-shard-00-00-fff0g.mongodb.net:27017,cluster0-shard-00-01-fff0g.mongodb.net:27017,cluster0-shard-00-02-fff0g.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
-}*/
-urlDB = 'mongodb+srv://admin:admin@cluster0-fff0g.mongodb.net/cafe'
+    urlDB = 'mongodb+srv://admin:admin@cluster0-fff0g.mongodb.net/cafe'
+}
+//urlDB = 'mongodb+srv://admin:admin@cluster0-fff0g.mongodb.net/cafe'
 process.env.URLDB = urlDB;
